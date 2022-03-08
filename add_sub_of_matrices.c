26dd9f5588ab0b0ec83c65fd24a49a5473bc07ca
AnshRithik <80160587+anshrithik@users.noreply.github.com>
2022-03-08 10:31:51 +0530

    Update add_sub_of_matrices.c

--- add_sub_of_matrices.c
+++ add_sub_of_matrices.c
@@ -1,62 +1,25 @@
 #include <stdio.h>
 
-void addMatrices(int m, int n, int a[m][n], int b[m][n]){
-    
-    int i, j , sum[m][n];
-    
-    for(i=0; i<m; i ++)
-        for(j=0; j<n; j++)
-            sum[i][j] = a[i][j] + b[i][j];
-            
-    printf("\n Sum of two matrices: \n");
-    for(i=0; i<m; i++){
-        for(j=0; j<n; j++){
-            printf("%d ", sum[i][j]);
-        }
-        printf("\n");
-    }
-    
-    
-}
+void main()	{
 
-void subMatrices(int m, int n, int a[m][n], int b[m][n]){
+	int m,n;
+	
+	printf("Enter Order of the matrices: ");
+  	scanf("%d %d", &m, &n);
+  	
+    int a[5][5],b[5][5];
+    int i,j;
     
-    int i, j , sum[m][n];
-    
-    for(i=0; i<m; i ++)
-        for(j=0; j<n; j++)
-            sum[i][j] = a[i][j] - b[i][j];
-            
-    printf("\n Sum of two matrices: \n");
-    for(i=0; i<m; i++){
-        for(j=0; j<n; j++){
-            printf("%d ", sum[i][j]);
-        }
-        printf("\n");
-    }
-    
-    
-}
-
-void readElements(int m, int n, int a[m][n], int b[m][n]){
-    
-    int i, j;
-        
     printf("\n Enter %d elements of the first matrix row-wise: \n", m*n);
-    for(i=0; i<m; i++)
-        for(j=0; j<n; j++)
-            scanf("%d", &a[i][j]);
-
+    for(i=0;i<m;i++)
+        for(j=0;j<n;j++)
+            scanf("%d",&a[i][j]);
+   
     printf("\n Enter %d elements of the Second matrix row-wise: \n", m*n);
      for(i=0; i<m; i++)
         for(j=0; j<n; j++)
             scanf("%d", &b[i][j]);
-}
-
-void displayMatrices(int m, int n, int a[m][n], int b[m][n]){
     
-    int i, j;
-            
     printf("\n Stored matrices are: \n");
     
     printf("First matrix is: \n");
@@ -66,7 +29,7 @@ void displayMatrices(int m, int n, int a[m][n], int b[m][n]){
         }
         printf("\n");
     }
-
+    
     printf("Second matrix is: \n");
     for(i=0; i<m; i++){
         for(j=0; j<n; j++){
@@ -75,20 +38,34 @@ void displayMatrices(int m, int n, int a[m][n], int b[m][n]){
         printf("\n");
     }
     
-}
-
-void main() {
+    int sum[5][5];
+    
+    for(i=0;i<m;i++)	{
+        for(j=0;j<n; j++)	{
+            sum[i][j] = a[i][j] + b[i][j];
+    	}
+	}
 	
-  int m,n,i,j;
-  
-  printf("Enter Order of the matrices: ");
-  scanf("%d %d", &m, &n);
-  
-  int a[m][n] , b[m][n];
+    printf("\n Addition of two matrices: \n");
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++){
+            printf("%d ",sum[i][j]);
+        }
+        printf("\n");
+    }
 
-readElements(m,n,a,b);
-displayMatrices(m,n,a,b);
-addMatrices(m,n,a,b);
-subMatrices(m,n,a,b);
-
+    for(i=0;i<m;i++)	{
+        for(j=0;j<n;j++)	{
+            sum[i][j] = a[i][j] - b[i][j];
+    	}
+	}
+	
+    printf("\n Subtaction of two matrices: \n");
+    for(i=0; i<m; i++){
+        for(j=0; j<n; j++){
+            printf("%d ",sum[i][j]);
+        }
+        printf("\n");
+    }
+    
 }
